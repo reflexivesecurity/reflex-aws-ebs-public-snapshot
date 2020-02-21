8864cbb4c1e4ee6dbf629f5de8b5d58c83f989cb
Max Anderson <maxwell.c.anderson@gmail.com>
2020-02-21 14:07:38 -0800

    Initial commit

---
+++ enforce_no_public_ebs_snapshot.tf
@@ -0,0 +1,54 @@
+module "enforce_no_public_ebs_snapshot" {
+  source           = "git@github.com:cloudmitigator/reflex.git//modules/cwe_lambda?ref=v0.0.1"
+  rule_name        = "EnforceNoPublicEBSSnapshot"
+  rule_description = "Rule to check if EBS snapshot is modified to be public"
+
+  event_pattern = <<PATTERN
+{
+  "detail-type": [
+    "AWS API Call via CloudTrail"
+  ],
+  "source": [
+    "aws.ec2"
+  ],
+  "detail": {
+    "eventSource": [
+      "ec2.amazonaws.com"
+    ],
+    "eventName": [
+      "ModifySnapshotAttribute"
+    ]
+  }
+}
+PATTERN
+
+  function_name            = "EnforceNoPublicEBSSnapshot"
+  source_code_dir          = "${path.module}/source"
+  handler                  = "enforce_no_public_ebs_snapshot.lambda_handler"
+  lambda_runtime           = "python3.7"
+  environment_variable_map = { SNS_TOPIC = module.enforce_no_public_ebs_snapshot.sns_topic_arn }
+  custom_lambda_policy     = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:DescribeSnapshotAttribute",
+        "ec2:ModifySnapshotAttribute"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+
+
+  queue_name    = "EnforceNoPublicEBSSnapshot"
+  delay_seconds = 0
+
+  target_id = "EnforceNoPublicEBSSnapshot"
+
+  topic_name = "EnforceNoPublicEBSSnapshot"
+  email      = var.email
+}
---
+++ source/enforce_no_public_ebs_snapshot.py
@@ -0,0 +1,56 @@
+""" Module for PublicAMIRule """
+
+import json
+
+import boto3
+
+from reflex_core import AWSRule
+
+
+class PublicEBSSnapshotRule(AWSRule):
+    """ AWS rule for ensuring non-public AMIs """
+
+    client = boto3.client("ec2")
+
+    def __init__(self, event):
+        super().__init__(event)
+
+    def extract_event_data(self, event):
+        """ Extract required data from the CloudWatch event """
+        self.raw_event = event
+        self.ebs_snapshot_id = event["detail"]["requestParameters"]["snapshotId"]
+
+    def resource_compliant(self):
+        """ Determines if the EBS snapshot is public, and if so returns False and True otherwise """
+        is_compliant = True
+        response = self.client.describe_snapshot_attribute(
+            Attribute="createVolumePermission", SnapshotId=self.ebs_snapshot_id
+        )
+
+        for permission in response["CreateVolumePermissions"]:
+            try:
+                if permission["Group"] == "all":
+                    is_compliant = False
+            except KeyError:
+                continue
+
+        return is_compliant
+
+    def remediate(self):
+        """ Makes the EBS snapshot private """
+        self.client.modify_snapshot_attribute(
+            Attribute="createVolumePermission",
+            SnapshotId=self.ebs_snapshot_id,
+            CreateVolumePermission={"Remove": [{"Group": "all"}]},
+        )
+
+    def get_remediation_message(self):
+        """ Returns a message about the remediation action that occurred """
+        return f"The EBS snapshot with ID: {self.ebs_snapshot_id} was made public. Public access has been disabled."
+
+
+def lambda_handler(event, _):
+    """ Handles the incoming event """
+    print(event)
+    rule = PublicEBSSnapshotRule(json.loads(event["Records"][0]["body"]))
+    rule.run_compliance_rule()
