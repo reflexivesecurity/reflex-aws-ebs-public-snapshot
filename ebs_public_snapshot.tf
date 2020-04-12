42e5d6ed9050be72bb66fc2feeb545a464dc9738
Chris Coleman <chris@rot-26.com>
2020-04-12 16:27:43 -0600

    Renamed tf: from `enforce_no_public_ebs_snapshot` to `ebs_public_snapshot`

--- ebs_public_snapshot.tf
+++ ebs_public_snapshot.tf
@@ -1,4 +1,4 @@
-module "enforce_no_public_ebs_snapshot" {
+module "ebs_public_snapshot" {
   source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=v0.5.7"
   rule_name        = "EnforceNoPublicEBSSnapshot"
   rule_description = "Rule to check if EBS snapshot is modified to be public"
@@ -24,7 +24,7 @@ PATTERN
 
   function_name            = "EnforceNoPublicEBSSnapshot"
   source_code_dir          = "${path.module}/source"
-  handler                  = "enforce_no_public_ebs_snapshot.lambda_handler"
+  handler                  = "ebs_public_snapshot.lambda_handler"
   lambda_runtime           = "python3.7"
   environment_variable_map = { SNS_TOPIC = var.sns_topic_arn }
   custom_lambda_policy     = <<EOF
