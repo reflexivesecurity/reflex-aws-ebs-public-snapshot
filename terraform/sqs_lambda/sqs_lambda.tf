4dd8b616c536a78d985d3c50565b5866815d9749
rjulian <richard@rjulian.net>
2020-04-30 16:25:40 -0700

    Fix path for lambda source

--- terraform/sqs_lambda/sqs_lambda.tf
+++ terraform/sqs_lambda/sqs_lambda.tf
@@ -4,7 +4,7 @@ module "sqs_lambda" {
   cloudwatch_event_rule_id  = var.cloudwatch_event_rule_id
   cloudwatch_event_rule_arn = var.cloudwatch_event_rule_arn
   function_name            = "EbsPublicSnapshot"
-  source_code_dir          = "${path.module}/source"
+  source_code_dir          = "${path.module}/../../source"
   handler                  = "ebs_public_snapshot.lambda_handler"
   lambda_runtime           = "python3.7"
   environment_variable_map = { SNS_TOPIC = var.sns_topic_arn }
