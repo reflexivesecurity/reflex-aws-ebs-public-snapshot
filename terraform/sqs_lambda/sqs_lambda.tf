ff2310ffdd3833c161684e09ccd4e61ff11f21e1
rjulian <richard@rjulian.net>
2020-05-12 15:34:55 -0700

    Patch: update pinned version for engine and formatting

--- terraform/sqs_lambda/sqs_lambda.tf
+++ terraform/sqs_lambda/sqs_lambda.tf
@@ -1,14 +1,14 @@
 module "sqs_lambda" {
-  source = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/sqs_lambda?ref=region-refactor"
+  source = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/sqs_lambda?ref=v0.6.0"
 
   cloudwatch_event_rule_id  = var.cloudwatch_event_rule_id
   cloudwatch_event_rule_arn = var.cloudwatch_event_rule_arn
-  function_name            = "EbsPublicSnapshot"
-  source_code_dir          = "${path.module}/../../source"
-  handler                  = "ebs_public_snapshot.lambda_handler"
-  lambda_runtime           = "python3.7"
-  environment_variable_map = { SNS_TOPIC = var.sns_topic_arn }
-  custom_lambda_policy     = <<EOF
+  function_name             = "EbsPublicSnapshot"
+  source_code_dir           = "${path.module}/../../source"
+  handler                   = "ebs_public_snapshot.lambda_handler"
+  lambda_runtime            = "python3.7"
+  environment_variable_map  = { SNS_TOPIC = var.sns_topic_arn }
+  custom_lambda_policy      = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -30,6 +30,6 @@ EOF
 
   target_id = "EbsPublicSnapshot"
 
-  sns_topic_arn = var.sns_topic_arn
+  sns_topic_arn  = var.sns_topic_arn
   sqs_kms_key_id = var.reflex_kms_key_id
 }
