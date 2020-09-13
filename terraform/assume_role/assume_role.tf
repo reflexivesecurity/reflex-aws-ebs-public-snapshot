e1d28758ac0880b4b26c8a086ea6dc25434b0c04
rjulian <richard@rjulian.net>
2020-09-13 14:46:29 -0700

    Semi-automated multi account push

---
+++ terraform/assume_role/assume_role.tf
@@ -0,0 +1,24 @@
+data "aws_caller_identity" "current" {}
+module "assume_role" {
+  source = "git::https://github.com/reflexivesecurity/reflex-engine.git//modules/sqs_lambda/modules/iam_assume_role?ref=v2.1.0"
+
+  function_name        = "EbsPublicSnapshot"
+  custom_lambda_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:DescribeSnapshotAttribute",
+        "ec2:ModifySnapshotAttribute"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+
+  lambda_execution_role_arn = "arn:aws:iam::${var.parent_account}:role/ReflexEbsPublicSnapshotLambdaExecution"
+
+}
